188b0464b340f4b99240cb8745a66e21430aa5b0
Rama Almer Felix <quenzvezda@gmail.com>
2023-09-17 22:13:13 +0700

    Add entry stock data

--- editData.php
+++ editData.php
@@ -8,6 +8,51 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $pdo = new PDO($dsn, $user, $pass);
 
+// Mengambil semua merk dari tabel merk
+$stmtMerk = $pdo->prepare("SELECT id, nama_merk FROM merk");
+$stmtMerk->execute();
+$all_merks = $stmtMerk->fetchAll(PDO::FETCH_ASSOC);
+
+$defaultMerkId = $all_merks[0]['id'];
+$stmtDefaultTipe = $pdo->prepare("SELECT id, nama_tipe FROM tipe WHERE id_merk = ?");
+$stmtDefaultTipe->execute([$defaultMerkId]);
+$default_tipes = $stmtDefaultTipe->fetchAll(PDO::FETCH_ASSOC);
+
+// Mengambil semua tipe dari tabel tipe
+$stmtTipe = $pdo->prepare("SELECT id, nama_tipe FROM tipe");
+$stmtTipe->execute();
+$all_tipes = $stmtTipe->fetchAll(PDO::FETCH_ASSOC);
+
+// Jika tombol 'add_stok' ditekan
+if (isset($_POST['add_stok'])) {
+    $merk = $_POST['merk'];
+    $tipe = $_POST['tipe'];
+    $harga = $_POST['harga'];
+    $ukuran = $_POST['ukuran'];
+    $jumlah_stok = $_POST['jumlah_stok'];
+
+    // Memasukkan data ke database
+    $stmt = $pdo->prepare("INSERT INTO stok (tipe, merk, harga, ukuran, jumlah_stok) VALUES (?, ?, ?, ?, ?)");
+    $stmt->execute([$tipe, $merk, $harga, $ukuran, $jumlah_stok]);
+
+    // Mengatur pesan sukses
+    $message = "Stok berhasil ditambahkan!";
+}
+
+// Jika permintaan untuk tipe berdasarkan merk diterima
+if (isset($_GET['get_tipes_for_merk'])) {
+    $merkId = $_GET['get_tipes_for_merk'];
+    
+    // Mengambil tipe-tipe berdasarkan merk yang dipilih
+    $stmt = $pdo->prepare("SELECT id, nama_tipe FROM tipe WHERE id_merk = ?");
+    $stmt->execute([$merkId]);
+    $tipes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    // Mengembalikan data dalam format JSON
+    echo json_encode($tipes);
+    exit();
+}
+
 // Mendapatkan data stok dari database
 $stmt = $pdo->prepare("
 SELECT 
@@ -59,7 +104,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['search'])) {
         <table>
             <thead>
                 <tr>
-                    <th>ID</th>
+                    <!-- <th>ID</th> -->
                     <th>Merk</th>
                     <th>Tipe</th>
                     <th>Harga</th>
@@ -71,7 +116,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['search'])) {
             <tbody>
                 <?php foreach($stok_data as $stok): ?>
                     <tr>
-                        <td><?= $stok['id'] ?></td>
+                        <!-- <td><?= $stok['id'] ?></td> -->
                         <td><?= $stok['merk_nama'] ?></td>
                         <td><?= $stok['tipe_nama'] ?></td>
                         <td><?= $stok['harga'] ?></td>
@@ -86,14 +131,63 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['search'])) {
                 <?php endforeach; ?>
             </tbody>
         </table>
-
-        <!-- Formulir Tambah Stok -->
+        <!-- Formulir Penambahan Stok -->
         <div class="add-form">
             <h3>Tambah Stok</h3>
-            <form>
-                <!-- Isi formulir untuk menambahkan stok baru akan ditempatkan di sini -->
+            <form action="editData.php" method="post">
+                <!-- Pilihan Merk -->
+                <label for="merk">Merk:</label>
+                <select name="merk" id="merk">
+                    <?php foreach ($all_merks as $merk): ?>
+                        <option value="<?= $merk['id'] ?>"><?= $merk['nama_merk'] ?></option>
+                    <?php endforeach; ?>
+                </select>
+                
+                <!-- Pilihan Tipe -->
+                <select name="tipe" id="tipe">
+                    <?php foreach ($default_tipes as $tipe): ?>
+                        <option value="<?= $tipe['id'] ?>"><?= $tipe['nama_tipe'] ?></option>
+                    <?php endforeach; ?>
+                </select>
+
+                <!-- Input Harga -->
+                <label for="harga">Harga:</label>
+                <input type="number" name="harga" id="harga" required>
+
+                <!-- Input Ukuran -->
+                <label for="ukuran">Ukuran:</label>
+                <input type="number" name="ukuran" id="ukuran" step="0.1" required>
+
+                <!-- Input Jumlah Stok -->
+                <label for="jumlah_stok">Jumlah Stok:</label>
+                <input type="number" name="jumlah_stok" id="jumlah_stok" required>
+
+                <!-- Tombol Submit -->
+                <input type="submit" name="add_stok" value="Tambah Stok">
             </form>
         </div>
     </div>
+    <!-- Skrip untuk memfilter tipe berdasarkan merk -->
+    <script>
+        document.getElementById('merk').addEventListener('change', function() {
+            let merkId = this.value;
+            
+            // Menggunakan fetch untuk meminta tipe-tipe berdasarkan merk yang dipilih
+            fetch(`editData.php?get_tipes_for_merk=${merkId}`)
+                .then(response => response.json())
+                .then(data => {
+                    let tipeDropdown = document.getElementById('tipe');
+                    tipeDropdown.innerHTML = ''; // Kosongkan dropdown tipe
+                    
+                    // Isi dropdown tipe dengan data yang diterima
+                    data.forEach(tipe => {
+                        let option = document.createElement('option');
+                        option.value = tipe.id;
+                        option.textContent = tipe.nama_tipe;
+                        tipeDropdown.appendChild(option);
+                    });
+                });
+        });
+    </script>
 </body>
 </html>
