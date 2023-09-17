cfb8818567e195b4467f55f0d4a5d7e02de112ec
Rama Almer Felix <quenzvezda@gmail.com>
2023-09-17 17:52:13 +0700

    add login form

---
+++ login.php
@@ -0,0 +1,120 @@
+<?php
+$message = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $host = 'localhost';
+    $db = 'sepatu';
+    $user = 'root'; // Ganti dengan username database Anda
+    $pass = ''; // Ganti dengan password database Anda
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try {
+        $pdo = new PDO($dsn, $user, $pass);
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+        $stmt->execute([$username, $password]);
+
+        if ($stmt->fetch()) {
+            header('Location: editData.php');
+            exit();
+        } else {
+            $message = 'Username atau Password salah!';
+        }
+    } catch (PDOException $e) {
+        $message = "Error: " . $e->getMessage();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        /* CSS yang mirip dengan main.php */
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-image: url('/resource/bg-main.jpg');
+            background-size: cover;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .login-container {
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 8px;
+            width: 300px;
+        }
+
+        .login-container h2 {
+            text-align: center;
+        }
+
+        .login-container form {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+
+        button, .back-button {
+            padding: 10px 20px;
+            font-size: 16px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+            margin-top: 10px;
+            text-align: center;
+            text-decoration: none;
+            display: block;
+            width: 100%;
+
+            box-sizing: border-box;
+        }
+
+        button {
+            background-color: #3498db;
+            color: white;
+        }
+
+        .back-button {
+            background-color: #e74c3c;
+            color: white;
+        }
+
+        button:hover, .back-button:hover {
+            opacity: 0.8;
+        }
+
+        .error {
+            color: red;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login</h2>
+        <?php if ($message): ?>
+            <p class="error"><?php echo $message; ?></p>
+        <?php endif; ?>
+        <form action="login.php" method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+        <a href="main.php" class="back-button">Kembali ke Halaman Utama</a>
+    </div>
+</body>
+</html>
