04047f44e98e915a75845f2a01cc1cb5cb5f0584
Rama Almer Felix <quenzvezda@gmail.com>
2023-09-19 21:41:03 +0700

    create function "logout"

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,6 +3,7 @@
         <li><a href="../tampil.php">Beranda</a></li>
         <li><a href="../operations/create.php">Tambah Stok</a></li>
         <li><a href="../operations/add_type.php">Tambah Tipe</a></li>
-        <li><a href="../operations/add_merk.php">Tambah Merk</a></li> <!-- Tambahkan baris ini -->
+        <li><a href="../operations/add_merk.php">Tambah Merk</a></li>
+        <li><a href="../index.php">Logout</a></li>
     </ul>
 </nav>
