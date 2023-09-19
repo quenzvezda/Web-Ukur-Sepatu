6e4ba4e66433f490c1a8186649636ad6db42fe69
Rama Almer Felix <quenzvezda@gmail.com>
2023-09-19 19:06:47 +0700

    add filter search by "merk"

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->execute([$username, $password]);
 
         if ($stmt->fetch()) {
-            header('Location: lihat_stok.php');
+            header('Location: tampil.php');
             exit();
         } else {
             $message = 'Username atau Password salah!';
--- tampil.php
+++ tampil.php
@@ -21,12 +21,24 @@ include "includes/config.php";
     </nav>
     <div class="container">
         <h1>Daftar Stok Sepatu</h1>
+        <form action="tampil.php" method="POST">
+            <select name="merk">
+                <option value="">Pilih Merk</option>
+                <?php
+                $merkQuery = $conn->query("SELECT * FROM merk");
+                while($merk = $merkQuery->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<option value='{$merk['id']}'>{$merk['nama_merk']}</option>";
+                }
+                ?>
+            </select>
+            <input type="submit" value="Cari">
+        </form>
         <table border="1" cellspacing="0" cellpadding="10">
             <thead>
                 <tr>
                     <th>ID</th>
-                    <th>Tipe</th>
                     <th>Merk</th>
+                    <th>Tipe</th>
                     <th>Harga</th>
                     <th>Ukuran</th>
                     <th>Jumlah Stok</th>
@@ -35,17 +47,20 @@ include "includes/config.php";
             </thead>
             <tbody>
                 <?php
+                // Cek apakah user memilih merk dari dropdown
+                $merkFilter = isset($_POST['merk']) && $_POST['merk'] != "" ? "WHERE s.merk = " . $_POST['merk'] : "";
+
                 $stmt = $conn->prepare("SELECT s.id, t.nama_tipe, m.nama_merk, s.harga, s.ukuran, s.jumlah_stok FROM stok s 
                                         JOIN tipe t ON s.tipe = t.id 
-                                        JOIN merk m ON s.merk = m.id");
+                                        JOIN merk m ON s.merk = m.id " . $merkFilter);
                 $stmt->execute();
 
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 ?>
                     <tr>
                         <td><?php echo $row['id']; ?></td>
-                        <td><?php echo $row['nama_tipe']; ?></td>
                         <td><?php echo $row['nama_merk']; ?></td>
+                        <td><?php echo $row['nama_tipe']; ?></td>
                         <td><?php echo $row['harga']; ?></td>
                         <td><?php echo $row['ukuran']; ?></td>
                         <td><?php echo $row['jumlah_stok']; ?></td>
